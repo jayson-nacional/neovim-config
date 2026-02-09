602f6c4f1936bcde37e3af38a579f0d680b14f27
Jayson Nacional <jayson.o.nacional@gmail.com>
2026-02-09 23:01:43 +0800

    omnisharp extended remaps

--- lua/config/mason-lsp-config.lua
+++ lua/config/mason-lsp-config.lua
@@ -34,14 +34,7 @@ vim.lsp.config('omnisharp', {
 		Sdk = {
 			IncludePrereleases = true,
 		},
-	},
-
-	handlers = {
-		["textDocument/definition"] = require('omnisharp_extended').definition_handler,
-		["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
-		["textDocument/references"] = require('omnisharp_extended').references_handler,
-		["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
-	},
+	}
 })
 
 vim.lsp.config('emmylua_ls', {
